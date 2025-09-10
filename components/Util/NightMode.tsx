426db8b162bd0dc7d8e556dd79f3f9ec56260f62
nbcgww <cuzknothz@gmail.com>
2025-09-10 14:24:04 +0700

    🙂

--- components/Util/NightMode.tsx
+++ components/Util/NightMode.tsx
@@ -1,29 +1,38 @@
 import MoonIcon from "@/assets/svg/moon.svg";
 import SunIcon from "@/assets/svg/sun.svg";
- 
+ 
 import { useGlobalStore } from "@/store/global-store";
-import { useEffect, useState } from "react";
- 
+import gsap from "gsap";
+import { useRef, useState } from "react";
+ 
 export const NightMode = () => {
-  const toggleDarkMode = useGlobalStore((state) => state.toggleDarkMode);
-  const [isDark, setIsDark] = useState(false);
-  useEffect(() => {
-    const htmlElement = document.documentElement;
-    toggleDarkMode(htmlElement.classList.contains("dark"));
-  }, [toggleDarkMode]);
- 
-  function toggle() {
-    const htmlElement = document.documentElement;
-    const isDark = htmlElement.classList.contains("dark");
-    htmlElement.classList[isDark ? "remove" : "add"]("dark");
-    setIsDark(isDark);
-  }
- 
-  return (
-    <div>
-      <button onClick={toggle} className="cursor-pointer">
-        {isDark ? <MoonIcon /> : <SunIcon />}
-      </button>
-    </div>
-  );
+  const toggleDarkMode = useGlobalStore((state) => state.toggleDarkMode);
+  const [isDark, setIsDark] = useState(false);
+  const containerRef = useRef<HTMLDivElement>(null);
+  function toggle() {
+    const tl = gsap.timeline({});
+    const handler = () => {
+      const htmlElement = document.documentElement;
+      const isDark = htmlElement.classList.contains("dark");
+      htmlElement.classList[isDark ? "remove" : "add"]("dark");
+      setIsDark(isDark);
+    };
+ 
+    tl.to(containerRef.current, {
+      duration: 0.5,
+      opacity: 0,
+      onComplete: handler,
+    }).to(containerRef.current, {
+      duration: 0.5,
+      opacity: 1,
+    });
+  }
+ 
+  return (
+    <div>
+      <button onClick={toggle} className="cursor-pointer">
+        <div ref={containerRef}>{isDark ? <MoonIcon /> : <SunIcon />}</div>
+      </button>
+    </div>
+  );
 };
