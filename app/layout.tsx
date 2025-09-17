18560679bcbf0d04498fbe47b57655ede58e503a
kuz <cuzknothz@gmail.com>
2025-09-17 23:11:43 +0700

    🪱

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { Metadata, Viewport } from 'next';
 import './globals.css';
 import { Slashhhhhhhhhhhhhhhhhhhhhhhhh } from '@/components/Slashhhhhhhhhhhhhhhhhhhhhhhhh';
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from '@vercel/analytics/next';
 
 export const metadata: Metadata = {
   title: 'illuzion',
@@ -30,10 +31,11 @@ export default function RootLayout({
         {/* <Slashhhhhhhhhhhhhhhhhhhhhhhhh /> */}
         <Dizzle />
         {children}
+        <SpeedInsights />
+        <Analytics />
         <Miracle />
         <EnterSecret />
       </LayoutEffect>
-      <SpeedInsights />
     </>
   );
 }
