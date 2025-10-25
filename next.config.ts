516d220ad79d15296b904f4b07304945e8e468ce
kuz <cuzknothz@gmail.com>
2025-10-25 22:34:04 +0700

    😹

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,10 @@
-import type { NextConfig } from 'next';
+import type  NextConfig  from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: false,
   typescript: {
     ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
   },
   turbopack: {
     rules: {
