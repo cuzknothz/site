1fb23197fce67a5ab50d2a4574406ebf7b00a4f7
kuz <cuzknothz@gmail.com>
2025-11-26 23:33:52 +0700

    🐤

--- components/Spotify/App/Header.tsx
+++ components/Spotify/App/Header.tsx
@@ -4,17 +4,20 @@ import { SearchDivision } from './SearchDivision';
 
 export const Header = () => {
   return (
-    <div className='flex h-[64px] items-center justify-between bg-[#000] px-[20px] w-full'>
-      <SpotifyIcon className='h-[32px]! w-[32px]! invert' />
+    <div className='flex h-[64px] w-full items-center justify-between bg-[#000] px-[20px]'>
+      <button>
+        <SpotifyIcon className='h-[32px]! w-[32px]! invert' />
+      </button>
+
       <div className='flex gap-[8px]'>
         <button className='flex h-[48px]! w-[48px]! items-center justify-center rounded-[50%] bg-[#282828]'>
           <HomeIcon className='h-[24px]! w-[24px]! invert' />
         </button>
         <SearchDivision />
       </div>
-      <div className='flex h-[48px] items-center rounded-[24px] bg-[#fff] px-[32px] font-bold text-black!'>
+      <button className='flex h-[48px] items-center rounded-[24px] bg-[#fff] px-[32px] font-bold text-black!'>
         Log in
-      </div>
+      </button>
     </div>
   );
 };
--- components/Spotify/App/SearchDivision.tsx
+++ components/Spotify/App/SearchDivision.tsx
@@ -3,17 +3,17 @@ import SearchExploreIcon from '@/assets/svg/spotify/search-explore-icon.svg';
 
 export const SearchDivision = () => {
   return (
-    <div className='relative flex h-[48px] w-[478px] items-center justify-between rounded-[24px] bg-[#282828] px-[12px]'>
-      <div>
-        <SearchIcon className='h-[48px] w-[24px] invert' />
-      </div>
+    <div className='group relative flex h-[48px] w-[478px] items-center justify-between rounded-[24px] bg-[#282828] px-[12px] hover:bg-[#373737] duration-500'>
+      <button className='z-1'>
+        <SearchIcon className='h-[48px] w-[24px] fill-[#fff]' />
+      </button>
       <input
         placeholder='What do you want to play? '
-        className='absolute top-0 left-0 h-full w-full placeholder:text-[#B1B1B1] py-[12px] pl-[48px] pr-[96px] focus:outline-0 border-[2px] focus:border-[#fff] duration-500 border-transparent rounded-[24px]'
+        className='absolute top-0 left-0 h-full w-full rounded-[24px] border-[2px] border-transparent py-[12px] pr-[96px] pl-[48px] text-white! caret-white duration-500 placeholder:text-[#B1B1B1] focus:border-[#fff] focus:outline-0'
       />
-      <div>
+      <button>
         <SearchExploreIcon className='h-[24px] w-[24px] invert' />
-      </div>
+      </button>
     </div>
   );
 };
